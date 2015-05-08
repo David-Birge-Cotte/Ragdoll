ead9ab2ef809a85b2fa60c099ae04d6b5d2637e1
Fangh <toto.klein@laposte;net>
2015-05-09 00:03:36 +0200

    fiest proto

---
+++ Assets/ArmBehavior.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArmBehavior : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetKeyDown(KeyCode.A))
+		{
+			GetComponent<Rigidbody2D>().AddTorque(2,ForceMode2D.Impulse);
+		}
+	}
+}
