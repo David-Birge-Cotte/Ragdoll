792609be6dd52e434ccd43e45ffee2a8c83fb719
lg33 <louis.gaillard@live.fr>
2015-05-09 21:55:46 +0200

    added menu pause

--- Assets/Scripts/_Manager.cs
+++ Assets/Scripts/_Manager.cs
@@ -5,7 +5,8 @@ using UnityEngine.UI;
 public class _Manager : MonoBehaviour {
 
 	public int Score;	
-	public Text ScoreUI;
+	public Text ScoreUI, soundButton;
+	public GameObject pausePanel;
 	GameObject[] listeCollectibles;
 
 	GameObject player;
@@ -26,10 +27,47 @@ public class _Manager : MonoBehaviour {
 		ScoreUI.text = "Score : " + Score.ToString() + "/" + listeCollectibles.Length.ToString();
 	}
 
-	public void restartLevel()
+	public void RestartLevel()
 	{
 		Debug.Log("Restart level");
 		player.transform.position = initialPlayerPos;
 		Application.LoadLevel(Application.loadedLevel);
+	}
+
+	public void SetPause()
+	{
+		Time.timeScale = 0;
+		pausePanel.SetActive(true);
+	}
+	public void Resume()
+	{
+		Time.timeScale = 1;
+		pausePanel.SetActive(false);
+	}
+
+	public void GoToEditor()
+	{
+		Destroy( player );
+		Application.LoadLevel("CharacterCreation");
+	}
+
+	bool soundActive = true;
+	public void SwitchSound()
+	{
+		if( soundActive )
+		{
+			Camera.main.GetComponent<AudioSource>().mute = soundActive = false;
+			soundButton.text = "Sound : OFF";
+		}
+		else
+		{
+			Camera.main.GetComponent<AudioSource>().mute = soundActive = true;
+			soundButton.text = "Sound : ON";
+		}
+	}
+
+	public void QuitGame()
+	{
+		Application.Quit();
 	}
 }
