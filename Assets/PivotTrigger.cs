9b119957b4fe9fdfe498ecc6abc0c437bdc1a177
Fangh <toto.klein@laposte;net>
2015-05-09 16:36:58 +0200

    debug

--- Assets/PivotTrigger.cs
+++ Assets/PivotTrigger.cs
@@ -5,8 +5,9 @@ public class PivotTrigger : MonoBehaviour {
 
     GameObject UIManager;
 	// Use this for initialization
-	void Start () {
-	UIManager = GameObject.Find("UIManager");
+	void Start () 
+    {
+	   UIManager = GameObject.Find("UIManager");
 	}
 	
 	// Update is called once per frame
@@ -17,11 +18,10 @@ public class PivotTrigger : MonoBehaviour {
     void OnTriggerEnter2D(Collider2D col)
     {
 
-        Debug.Log("Trigger");
         if (col.tag == "Pivot")
         {
-            Debug.Log("Hello");
             UIManager.GetComponent<Parts_GUI_Manager_Script>().DropObject(col.transform);
+            this.enabled = false;
         }
     }
 }
