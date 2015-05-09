223b4c967c2407ebd5e21848532653690540567d
PrTesla <t.totota@gmail.com>
2015-05-09 14:26:33 +0200

    Script Camera

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	public GameObject target;
+	public Vector3 offset;
+
+
+	// Use this for initialization
+	void Start () {
+		transform.position = target.transform.position + offset;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 pos;
+		pos = target.transform.position;
+		pos.y = 0;
+		pos += offset;
+
+		transform.position = pos;
+	
+	}
+}
