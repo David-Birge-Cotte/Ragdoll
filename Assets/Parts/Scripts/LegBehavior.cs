ca3bf10aa37d8e8c5f42c8d71606308de659f0e1
PrTesla <t.totota@gmail.com>
2015-05-09 11:01:52 +0200

    Leg and Folders

---
+++ Assets/Parts/Scripts/LegBehavior.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LegBehavior : MonoBehaviour {
+
+	private Vector3 m_scale;
+	private Vector3 m_newScale;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		m_scale = transform.localScale;
+		m_newScale = new Vector3(m_scale.x, m_scale.y * 3, m_scale.z);
+
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+
+		if(Input.GetKey(KeyCode.Space)) //NiceToHave : Utiliser l'Input Manager pour pouvoir le regler in-game / dans le launcher Unity.
+		{
+			transform.localScale = m_newScale;
+		} else {
+			transform.localScale = m_scale;
+		}
+	
+	}
+}
