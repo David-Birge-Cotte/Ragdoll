8cef451b9675de895f4291016c525f9606c59ac0
PrTesla <t.totota@gmail.com>
2015-05-09 13:36:14 +0200

    Legs

---
+++ Assets/Scripts/FootTestCollision.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FootTestCollision : MonoBehaviour {
+
+	public bool IsOnFloor;
+
+	void Update(){
+		Debug.Log(IsOnFloor);
+	}
+
+	void OnCollisionStay2D(Collision2D other)
+	{
+		if(other.gameObject.tag == "platform")
+			IsOnFloor = true;
+
+	}
+
+	void OnCollisionExit2D()
+	{
+		IsOnFloor = false;
+	}
+}
