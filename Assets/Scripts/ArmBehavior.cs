0f85a6796ab4fb560ff842758193de7cc9e94304
Fangh <toto.klein@laposte;net>
2015-05-09 11:52:28 +0200

    fix des conneries de David

--- Assets/Scripts/ArmBehavior.cs
+++ Assets/Scripts/ArmBehavior.cs
@@ -1,1 +1,21 @@
-﻿Trololol
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArmBehavior : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetKey(KeyCode.A))
+		{
+			GetComponent<Rigidbody2D>().AddTorque(20,ForceMode2D.Force);
+		}
+	}
+}
---
+++ Assets/Scripts/LambsManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class LambsManager : MonoBehaviour 
+{
+	public List<GameObject> pivots;
+
+	public GameObject selectedLamb;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if ( selectedLamb == null )
+		{
+
+		}
+		else
+		{
+			if( Input.GetKeyDown(KeyCode.W) ) 
+			{
+
+			}			
+		}
+	}
+}
