b885b894f06b7a0650900cdef97e445899cca88b
PrTesla <t.totota@gmail.com>
2015-05-10 11:55:34 +0200

    Bug Fix CameraCreation / BackGround

--- Assets/Scripts/CharacterCreationCamera.cs
+++ Assets/Scripts/CharacterCreationCamera.cs
@@ -3,13 +3,19 @@ using System.Collections;
 
 public class CharacterCreationCamera : MonoBehaviour 
 {
+	public GameObject BG;
+	public float adaptation;
+
 
 	// Update is called once per frame
 	void Update () 
 	{
 		if ( GetComponent<Camera>().orthographicSize - Input.GetAxis("Mouse ScrollWheel")  > 2 && GetComponent<Camera>().orthographicSize - Input.GetAxis("Mouse ScrollWheel") < 15)
 		{
-			GetComponent<Camera>().orthographicSize -= Input.GetAxis("Mouse ScrollWheel");			
+			GetComponent<Camera>().orthographicSize -= Input.GetAxis("Mouse ScrollWheel");
+			BG.transform.localScale = new Vector3(GetComponent<Camera>().orthographicSize * adaptation, GetComponent<Camera>().orthographicSize * adaptation, 1);
+
+
 		}
 	}
 }
